175d171cc57dfa98cab47cd339bcffa3082824df
sv022 <ssvatkin04@gmail.com>
2024-10-24 14:20:35 +0300

    switch to working version

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,21 +1,93 @@
-class Neuron
-{
-    private:
-    double activation;
+#pragma once
+#include<vector>
+#include <cmath>
 
-    public:
-    Neuron(){
-        activation = 0;
-    }
-    Neuron(double value){
-        activation = value;
-    }
 
-    double get(){
-        return activation;
-    }
+class Neuron;
+typedef std::vector<Neuron> Layer;
 
-    void set(double value){
-        activation = value;
-    }
+struct Connection {
+	double weight;
+	double deltaWeight;
 };
+
+class Neuron {
+
+private:
+	static double learningRate;
+	static double alpha;
+	//static double activate(double value) { return tanh(value); }
+	static double activate(double value) { return 1 / (1 + exp(-value)); }
+	//static double activateDerivative(double value) { return 1 - tanh(value) * tanh(value); }
+	static double activateDerivative(double value) { return activate(value) * (1 - activate(value)); }
+	static double random(void) { return rand() / double(RAND_MAX); }
+	double sumDOW(const Layer &nextLayer) const;
+	double output;
+	std::vector<Connection> outputWeights;
+	unsigned index;
+	double gradient;
+
+public:
+	Neuron(unsigned outputSize, unsigned index);
+	void setOutput(double value) { output = value; }
+	double getOutput(void) const { return output; }
+	std::vector<Connection> getOutputWeights() const { return outputWeights; }
+	void feedForward(const Layer &prevLayer);
+	void calculateOutputGradients(double target);
+	void calculateHiddenGradients(const Layer &nextLayer);
+	void updateWeights(Layer &prevLayer);
+
+};
+
+double Neuron::learningRate = 0.2;
+double Neuron::alpha = 0.5;
+
+Neuron::Neuron(unsigned outputSize, unsigned index) {
+	this->index = index;
+	outputWeights.reserve(outputSize);
+
+	for (unsigned i = 0; i < outputSize; i++) {
+		outputWeights.push_back(Connection());
+		outputWeights.back().weight = random();
+	}
+}
+
+void Neuron::updateWeights(Layer &prevLayer) {
+	for (unsigned n = 0; n < prevLayer.size(); n++) {
+		double oldDeltaWeight = prevLayer[n].outputWeights[index].deltaWeight;
+
+		double newDeltaWeight = learningRate * prevLayer[n].getOutput() * gradient + alpha * oldDeltaWeight;
+
+		prevLayer[n].outputWeights[index].deltaWeight = newDeltaWeight;
+		prevLayer[n].outputWeights[index].weight += newDeltaWeight;
+	}
+}
+
+double Neuron::sumDOW(const Layer &nextLayer) const {
+	double sum = 0.0;
+
+	for (unsigned n = 0; n < nextLayer.size() - 1; n++)
+		sum += outputWeights[n].weight * nextLayer[n].gradient;
+
+	return sum;
+}
+
+void Neuron::calculateHiddenGradients(const Layer &nextLayer) {
+	double dow = sumDOW(nextLayer);
+	gradient = dow * Neuron::activateDerivative(output);
+}
+
+void Neuron::calculateOutputGradients(double target) {
+	double delta = target - output;
+	gradient = delta * Neuron::activateDerivative(output);
+}
+
+void Neuron::feedForward(const Layer &prevLayer) {
+	double sum = 0.0;
+
+	for (unsigned n = 0; n < prevLayer.size(); n++)
+		sum += prevLayer[n].getOutput() * prevLayer[n].outputWeights[index].weight;
+
+	output = Neuron::activate(sum);
+}
+
